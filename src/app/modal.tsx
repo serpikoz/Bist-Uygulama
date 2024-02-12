bd017f1b7426126f6b407d74f3afdc9022df5833
Kadir <116947551+serpikoz@users.noreply.github.com>
2024-02-12 13:17:21 +0300

    demo-modal-finish

--- src/app/modal.tsx
+++ src/app/modal.tsx
@@ -3,41 +3,79 @@ import { Platform, StyleSheet } from "react-native";
 
 import { Text, View } from "../components/Themed";
 import { MonoText } from "../components/StyledText";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList, ScrollView } from "react-native-gesture-handler";
 
 export default function ModalScreen() {
   return (
-    <View style={styles.container}>
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {/* Use a light status bar on iOS to account for the black space above the modal */}
 
-      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+        <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
 
-      <Text style={styles.headerfirst}>Borsa Guru Nedir ?</Text>
-      <View style={{ borderBottomWidth: 3, borderColor: "#47315a" }}>
-        <Text style={styles.first}>
-          Borsa dünyasına adım atmak isteyenler için tasarlanmış bu uygulama,
-          gerçek para kullanmadan hisse senetlerinin değerlerindeki artış ve
-          azalışı izlemenize olanak tanıyor.
-        </Text>
-      </View>
-      <Text style={styles.headerfirst}>Neden Biz ?</Text>
-      <View style={{ borderBottomWidth: 3, borderColor: "#47315a" }}>
-        <Text style={styles.two}>
-          <MonoText style={{ fontWeight: "bold" }}>
-            💡Öğrenme Fırsatı:{" "}
-          </MonoText>
-          Borsa dünyasını keşfetmek isteyenler için mükemmel bir öğrenme
-          platformu sunuyoruz. Uygulamamızı kullanarak, finansal piyasalar
-          hakkında daha fazla bilgi edinebilir ve yatırım stratejilerinizi
-          geliştirebilirsiniz
-        </Text>
-        <Text style={styles.two}>
-          <MonoText style={{ fontWeight: "bold" }}>👍 Risk Yok: </MonoText>
-          Uygulamamızda, gerçek para kullanmadan sanal bir borsa deneyimi
-          yaşarsınız.Bu sayede, finansal kararlar alırken risk almadan deneyim
-          kazanabilirsiniz.
-        </Text>
-      </View>
-    </View>
+        {/* Borsa Guru Nedir  */}
+
+        <View style={{ borderBottomWidth: 3, borderColor: "#47315a" }}>
+          <Text style={styles.headerfirst}>Borsa Guru Nedir ?</Text>
+          <Text style={styles.first}>
+            Borsa dünyasına adım atmak isteyenler için tasarlanmış bu uygulama,
+            gerçek para kullanmadan hisse senetlerinin değerlerindeki artış ve
+            azalışı izlemenize olanak tanıyor.
+          </Text>
+        </View>
+
+        {/* Neden Biz */}
+
+        <View style={{ borderBottomWidth: 3, borderColor: "#47315a" }}>
+          <Text style={styles.headerfirst}>Neden Biz ?</Text>
+          <Text style={styles.two}>
+            <MonoText style={{ fontWeight: "bold" }}>
+              💡Öğrenme Fırsatı:{" "}
+            </MonoText>
+            Borsa dünyasını keşfetmek isteyenler için mükemmel bir öğrenme
+            platformu sunuyoruz. Uygulamamızı kullanarak, finansal piyasalar
+            hakkında daha fazla bilgi edinebilir ve yatırım stratejilerinizi
+            geliştirebilirsiniz
+          </Text>
+          <Text style={styles.two}>
+            <MonoText style={{ fontWeight: "bold" }}>👍 Risk Yok: </MonoText>
+            Uygulamamızda, gerçek para kullanmadan sanal bir borsa deneyimi
+            yaşarsınız.Bu sayede, finansal kararlar alırken risk almadan deneyim
+            kazanabilirsiniz.
+          </Text>
+        </View>
+
+        {/* Kayıt Olma Yeri */}
+
+        <View>
+          <Text style={styles.headerfirst}>Nasıl Kullanılır ?</Text>
+          <Text style={styles.two}>
+            <MonoText style={{ fontWeight: "bold" }}>1-Kayıt Olun: </MonoText>
+            Borsa dünyasını keşfetmek isteyenler için mükemmel bir öğrenme
+            platformu sunuyoruz. Uygulamamızı kullanarak, finansal piyasalar
+            hakkında daha fazla bilgi edinebilir ve yatırım stratejilerinizi
+            geliştirebilirsiniz
+          </Text>
+          <Text style={styles.two}>
+            <MonoText style={{ fontWeight: "bold" }}>
+              2-Sanal Bakiye Alın:{" "}
+            </MonoText>
+            Uygulamamızda, gerçek para kullanmadan sanal bir borsa deneyimi
+            yaşarsınız.Bu sayede, finansal kararlar alırken risk almadan deneyim
+            kazanabilirsiniz.
+          </Text>
+          <Text style={styles.two}>
+            <MonoText style={{ fontWeight: "bold" }}>
+              3-Hisse Senetleri Satın Alın:{" "}
+            </MonoText>
+            Uygulamamızda, gerçek para kullanmadan sanal bir borsa deneyimi
+            yaşarsınız.Bu sayede, finansal kararlar alırken risk almadan deneyim
+            kazanabilirsiniz.
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -45,16 +83,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  scrollView: {
+    marginHorizontal: 3,
+  },
 
   headerfirst: {
     textAlign: "center",
-    marginTop: 18,
+    marginTop: 10,
     fontSize: 20,
+    textTransform: "uppercase",
+    fontWeight: "bold",
   },
   first: {
     marginTop: 8,
     fontSize: 16,
-    padding: 10,
+    padding: 8,
     textAlign: "center",
     lineHeight: 20,
     borderBottomWidth: 5,
@@ -63,7 +106,7 @@ const styles = StyleSheet.create({
 
   two: {
     fontSize: 16,
-    padding: 10,
+    padding: 8,
     lineHeight: 20,
     borderBottomWidth: 5,
     borderBottomColor: "#fff",
